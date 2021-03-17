ac19106ceafa2386d6c85796b178af28235a0735
the-apprentice <theapprentice@ta.com>
2021-03-17 15:17:24 +0000

    fixed length error

--- asm/x86/02_data_section.asm
+++ asm/x86/02_data_section.asm
@@ -5,8 +5,8 @@ section .data
 
 ; one byte
 
-bone db "a", 0xA		; db = declare byte (referred by variable bone and value is "a")
-len1 equ 2                      ; length (in bytes)
+bone db "a", 1001b, "b", 0Ah	; db = declare byte (referred by variable bone and value is "a")
+len1 equ 4                      ; length (in bytes)
 
 btwo db  90, 0xA, 0xA
 len2 equ 3
@@ -22,12 +22,35 @@ section .text
 _start:
 	nop
 	
-	mov eax, 0x04		; print byte (doesnt work without message length)
-	mov ebx, 1
-	mov ecx, bfr
-	mov edx, len4
-	int 0x80	
+	jmp print_bone
+	; jmp print_btwo
+	; jmp print_bfr
 	
-	mov eax, 1		; quit
-	mov ebx, 0
-	int 80h
+	print_bone:
+		mov eax, 0x04		; print byte (doesnt work without message length)
+		mov ebx, 1
+		mov ecx, bone
+		mov edx, len1
+		int 0x80	
+		jmp exit
+		
+	print_btwo:
+		mov eax, 0x04		; print byte (doesnt work without message length)
+		mov ebx, 1
+		mov ecx, bfr
+		mov edx, len2
+		int 0x80	
+		jmp exit		
+
+	print_bfr:
+		mov eax, 0x04		; print byte (doesnt work without message length)
+		mov ebx, 1
+		mov ecx, bfr
+		mov edx, len4
+		int 0x80	
+
+
+	exit:			
+		mov eax, 1		; quit
+		mov ebx, 0
+		int 80h
