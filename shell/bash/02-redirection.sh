9cb5635cde2e0ebb2e9abfcac8ddd22bc144e50d
the-apprentice <theapprentice@ta.com>
2021-03-19 11:45:07 +0000

    added redirection

---
+++ shell/bash/02-redirection.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cat >> to a file
+# echo "line" >> to a file
+# echo "overwrite whole file" > file
+
+# cat << mlc_start "whatever typed here to terminal
+#		   "or can be from a file" mlcstart
+# cat file (to stdout)
