cdd21c73c3b484484fd48886ebc3a32a46f64c41
the-apprentice <theapprentice@ta.com>
2021-03-19 20:42:51 +0000

    c

---
+++ c/01-preproc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>			// <file> searches in system path
+#include "01-preproc.h"			// "file.h" searches in local dir first
+
+#include "01-preproc.h"			// will only be included once
+
+int main(){
+
+}
---
+++ c/01-preproc.h
@@ -0,0 +1,9 @@
+#ifndef preproc_header
+#define preproc_header
+
+void method1();
+void method2(int a, int b);
+int  method3();
+
+
+#endif 
