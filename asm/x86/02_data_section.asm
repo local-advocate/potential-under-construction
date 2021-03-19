95b9dcdfa3a3f6c2b4c9bdac4079694c74045ca6
the-apprentice <theapprentice@ta.com>
2021-03-19 14:33:19 +0000

    sections good

--- asm/x86/02_data_section.asm
+++ asm/x86/02_data_section.asm
@@ -1,58 +1,74 @@
 global _start
 
-; static/global
-section .data
+
+;---------------data-------------------------
+
+section .data 					; user for constant data 
+
+prompt db   "Enter you name: "			; db = declare byte
+len1   equ  $ - prompt                    	; length (in bytes)
+
+respon db   "Your name is "
+rpnln  equ  $ - respon
+
+newline db 0xA
 
 
-; byte operations
-bone db "a", 1001b, "b", 0Ah	; db = declare byte (referred by variable bone and value is "a")
-len1 equ 4                      ; length (in bytes)
+;---------------bss-------------------------
 
-btwo db  90, 0xA, 0xA
-len2 equ 3
+section .bss  					; user for variables/uninitialized data
 
-; btre .byte 			; error instruction expected if left empty 
-; btre: .byte			; illgal cant use labels
+input  resb 100					; reserve 100 bytes 
 
-bfr db "ha",  0xA
-len4 equ 3
 
-; .short, .long, .string
+;----------------text----------------------
  	
 section .text
 
 _start:
-	nop
-	
-	jmp print_bone
-	; jmp print_btwo
-	; jmp print_bfr
-	
-	print_bone:
-		mov eax, 0x04		; print byte (doesnt work without message length)
+
+	print_prompt:
+		mov eax, 0x04		
 		mov ebx, 1
-		mov ecx, bone
+		mov ecx, prompt
 		mov edx, len1
 		int 0x80	
-		jmp exit
+
+	read_input:
+		mov eax, 3			; syscall number for read
+		mov ebx, 2			; read from stdin
+		mov ecx, input			; buf to store at
+		mov edx, 100			; size of input
+		int 0x80
+
+	print_response:
+		mov eax, 0x04	
+		mov ebx, 1
+		mov ecx, respon
+		mov edx, rpnln
+		int 0x80
+
+	print_input:
+		mov eax, 0x04
+		mov ebx, 1
+		mov ecx, input
+		mov edx, 100
+		int 0x80
+	
+	print_newline:
+		mov eax, 0x04
+		mov ebx, 1
+		mov ecx, newline
+		mov edx, 1
+		int 0x80	
+
+	; print_something:			; add later macro/call to library func
+	;	mov eax, 0x04
+	;	mov ebx, 1
+	;	int 0x80
 		
-	print_btwo:
-		mov eax, 0x04		; print byte (doesnt work without message length)
-		mov ebx, 1
-		mov ecx, bfr
-		mov edx, len2
-		int 0x80	
-		jmp exit		
-
-	print_bfr:
-		mov eax, 0x04		; print byte (doesnt work without message length)
-		mov ebx, 1
-		mov ecx, bfr
-		mov edx, len4
-		int 0x80	
-
 
 	exit:			
-		mov eax, 1		; quit
+		mov eax, 1			; quit
 		mov ebx, 0
 		int 80h
