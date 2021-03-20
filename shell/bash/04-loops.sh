3968e17f0f222a23780ffbd59f3d81338e93b708
the-apprentice <theapprentice@ta.com>
2021-03-20 11:11:02 +0000

    added loops

---
+++ shell/bash/04-loops.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+i=0
+
+# ------------WHILE-------------
+echo "WHILE LOOP"
+while (( $i < 10 ))			# [ $i < 1] file op. not while = until
+do
+	echo $i
+	i=$(( i+1 ))			# $i+1 string op. 
+done
+
+# -----------FOR---------------
+echo "FOR LOOP (python)"		# {start..end..step}
+for i in {0..10..2}
+do
+	echo $i
+done
+
+echo "FOR LOOP (c)"
+for (( i=0; i<=10; i++ ))
+do
+	echo $i
+done
