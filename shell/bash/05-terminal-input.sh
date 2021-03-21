cd30e02fef476f6d4ab5a4d99a4c611ad579ccf4
the-apprentice <theapprentice@ta.com>
2021-03-21 11:49:56 +0000

    terminal input

---
+++ shell/bash/05-terminal-input.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# echo $0 $1 $2 $3			# $0: file name and the consecutive inputs
+
+arg_arr=("$@")				# array of @ (unlimited) size
+
+<<useless
+i=0
+for (( i = 0; i < $#; i++ ))		# $#: length of array/args
+do
+	echo ${arg_arr[i]}
+done
+useless
+
+echo $@					# print all args
