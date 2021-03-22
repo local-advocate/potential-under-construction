b74b8322473b8f63e367d6fff6972568ddf031ff
the-apprentice <theapprentice@ta.com>
2021-03-22 10:05:49 +0000

    functions in bash

--- shell/bash/07-functions.sh
+++ shell/bash/07-functions.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 
-cont: https://youtu.be/e7BufAVwDiM?t=5738
+function func1() {
+	echo in func 1
+	echo arguments: $@
+	
+	return="func1 out"
+	echo func1 return: $return
+
+	func2 called by func1
+}
+
+function func2() {
+	echo
+	echo in func 2
+	echo arguments: $@
+	
+	return="func2 out"
+	echo func2 return: $return
+
+}
+
+func1 called from "main"
