8ab4d1b4136febea114b54fab98414d319d88b0f
the-apprentice <theapprentice@ta.com>
2021-03-22 11:01:25 +0000

    switch case modified

--- shell/bash/09-switch-case.sh
+++ shell/bash/09-switch-case.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 
-while [ $# -gt 0 ]; do
-	echo in while loop
-	case $1 in 
-		-1)
+if [ $# -eq 0 ]; then
+	echo usage: $0 -[1-4]\|--brk			# number of args = 0
+	exit
+fi
+
+
+while [ $# -gt 0 ]; do					# parse arguments
+	echo in while loop. argument = $1
+
+	case $1 in 					# switch (option 1) 
+
+		-1)					# case -1
 			echo option = -1
-			shift
-			;;
+			shift				# shift arguments to left by one
+			;;				# break (case)
 		-2)
 			echo option = -2
 			shift
@@ -23,10 +31,16 @@ while [ $# -gt 0 ]; do
 			echo breaking loop
 			break
 			;;
-		*)
+		*)					# default/case not found
 			echo no such option
-			break
+			break				# break (while loop)
 			;;
-	esac
 
-done
+	esac						# end switch-case (esac opp of case. if fi)
+
+	echo arguments $@ 				# after every switch case
+	echo
+done							# while loop done all args passed
+
+echo
+echo while loop done
