51a69cb9ec710bd07238bee27979b8226967c8e1
the-apprentice <theapprentice@ta.com>
2021-03-23 18:32:41 +0000

    more types and formatting

--- c/02-data-types.c
+++ c/02-data-types.c
@@ -5,11 +5,24 @@
 int main(){
 	printf("TYPE\t\t\tMIN\t\tMAX\n\n");
 
-	/*----INT TYPES----*/
-	int8_t   eights; //use with %PRId8 to print as signed. use with %PRIu8 to print as unsigned.
-	int16_t sixtins;
-	int32_t thrtwos;
-	int64_t  sixfrs;
+	/*----INT TYPES----*/	// (x in 8, 16, 32, 64).
+
+		
+	/*
+	 * --unsigned or signed (abs size)--
+	 *  MACROS:
+	 *  	unsigned: MAX: UNITx_MAX.
+	 * 	signed  : MIN:  INTx_MIN.  MAX: INTx_MAX.
+	 *  
+	 *  SPECIFIER:
+	 * 	%PRIuX to print as unsigned.
+	 * 	%PRIdX to print as signed. 
+	 *
+	 */
+	uint8_t  eightu;  int8_t    eights;
+	uint16_t sixtinu; int16_t  sixtins;
+	uint32_t thrtwou; int32_t  thrtwos;
+	uint64_t  sixfru; int64_t   sixfrs;
 
 	printf("uint8_t:\t\t%" PRIu8 "\t\t%" PRIu8 "\n", 0, UINT8_MAX); 
 	printf("int8_t: \t\t%" PRId8 "\t\t%" PRId8 "\n\n", INT8_MIN, INT8_MAX); 
@@ -24,4 +37,19 @@ int main(){
 	printf("int64_t:\t\t%" PRId64 "\t\t%" PRId64 "\n", INT64_MIN, INT64_MAX); 
 
 
+	/*
+	 * --least ints: atleast x size--
+	 *  MACROS:
+	 *  	unsigned: MAX: UINT_LEASTx_MAX.
+	 * 	signed  : MIN:  INT_LEASTx_MIN. MAX: UINT_LEASTx_MAX.
+	 *  
+	 *  SPECIFIER:
+	 * 	%PRIuLEASTx to print as unsigned.
+	 * 	%PRIdLEASTx to print as signed. 
+	 *
+	 */
+	uint_least8_t  atleasteights;   int_least8_t  atleasteights;
+	uint_least8_t  atleasteights;   int_least8_t  atleasteights;	
+	uint_least8_t  atleasteights;   int_least8_t  atleasteights;   
+	uint_least8_t  atleasteights;   int_least8_t  atleasteights;   
 }
