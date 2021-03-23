48c3b0e59b5fad262a20ae7139227acea770a517
the-apprentice <theapprentice@ta.com>
2021-03-23 15:27:22 +0000

    added comments

--- asm/x86/03_addressing.asm
+++ asm/x86/03_addressing.asm
@@ -7,6 +7,7 @@ temp2 db "array: "
 newline db 0xA
 
 array dd 0, 1, 2, 3, 4, 5		; array of bytes
+; array times 9 dd 0
 arraylen equ ($ - array)/4		; dd = 4 bytes
 
 section .bss
@@ -29,35 +30,40 @@ _start:
 	mov eax, [array+1*4]				; eax should be 2
 	mov eax, array + 8				; eax = ptr to 2nd element
 
-	mov esi, array
-	mov edi, arraylen - 1
+	;---------printing an array----------
+	mov esi, array					; esi = array[0] address
+	mov edi, arraylen - 1				; edi = len of arr
 
-	mov eax, 4
+	mov eax, 4					; print "array: "
 	mov ebx, 1
 	mov ecx, temp2
 	mov edx, 7
 	int 0x80
 
-	print_arr:
-		mov eax, 0x04
+	; learned from https://www.daniweb.com/programming/software-development/threads/488646/how-to-print-array-in-nasm
+	print_arr:	
+
+		mov ecx, [esi]				; ecx = first element of array
+		add ecx, 48				; to convert from int to ascii (cant print int)
+		push ecx				; push ecx to get address of ascii (for print syscall)
+		mov ecx, esp				; address of ascii
+
+		mov eax, 0x04				; print int
 		mov ebx, 1
-		mov ecx, [esi]
-		add ecx, 48
-		push ecx
-		mov ecx, esp
-		mov edx, 1
+		mov edx, 1			
 		int 0x80
-		pop ecx
 
-		add esi, 4
-		sub edi, 1
+		pop ecx					; stack restoring (can be ignored if no overflow)
 
-		cmp edi, 0
+		add esi, 4				; next array elem (dd = 4 byte)
+		sub edi, 1				; counter -= 1
+
+		cmp edi, 0				; loop if not end
 		jge print_arr
-		jmp exit
+		jmp exit				; else jump to exit
 
 	exit:
-		mov eax, 4
+		mov eax, 4				; print newline and exit
 		mov ebx, 1
 		mov ecx, newline
 		mov edx, 1
