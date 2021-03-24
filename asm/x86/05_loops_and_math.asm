2fa27e74aa89973003a48d74df13d89d2003dfc6
the-apprentice <theapprentice@ta.com>
2021-03-24 14:35:27 +0000

    added comments for loops in asm

--- asm/x86/05_loops_and_math.asm
+++ asm/x86/05_loops_and_math.asm
@@ -16,36 +16,35 @@ section .text
 
 _start:
 
-inc_loop:
-	cmp byte [counter], 10
-	je  inc_loop_end
+inc_loop:						; prints from 0 to 9 (including 9)
+	cmp byte [counter], 10				; if counter == 10, 
+	je  inc_loop_end				; exit the loop
 
-	mov eax, 4
+	mov eax, 4					; else print counter
 	mov ebx, 1
 
-	mov ecx, [counter]
+	mov ecx, [counter]				; convert from decimal to ascii
 	add ecx, 48
-	mov [value], ecx
+	mov [value], ecx				; save ascii in value for ecx pointer
 	mov ecx, value
 
 	mov edx, 1
-	int 0x80
+	int 0x80					; print ascii value of counter decimal
 
-	inc byte  [counter]
-	jmp inc_loop
+	inc byte  [counter]				; counter++
+	jmp inc_loop					; loop again
 
-inc_loop_end:
-	dec byte [counter]
-
-	mov eax, 4
+inc_loop_end:						; handles loop exit
+	dec byte [counter]				; decrement counter (last counter val = 10)
+	mov eax, 4					; print newline
 	mov ebx, 1
 	mov ecx, newline
 	mov edx, 1
 	int 0x80	
 
 
-dec_loop:
-	cmp byte [counter], -1
+dec_loop:						; same as inc_loop but print from 9 to 0
+	cmp byte [counter], -1				
 	je  dec_loop_end
 
 	mov eax, 4
@@ -73,11 +72,11 @@ dec_loop_end:
 arr_loop:
 
 exit:
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, newline
-	mov edx, 1
-	int 0x80	
+;	mov eax, 4
+;	mov ebx, 1
+;	mov ecx, newline
+;	mov edx, 1
+;	int 0x80	
 
 	mov eax, 1
 	mov ebx, 0
