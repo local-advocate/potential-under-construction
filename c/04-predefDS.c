f271dc69c2ef58fe802e05578270207af042b7f4
the-apprentice <theapprentice@ta.com>
2021-03-25 19:51:40 +0000

    trying to print arrays

--- c/04-predefDS.c
+++ c/04-predefDS.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main(){
-	printf("revisiting structures and unions");
+
+	/*----ARRAYS----*/
+	int intarr[5] = {0, 1, 2, 3, 4};
+	double doubarr[] = {1.1, 2.22, 3.333, 4.4444};
+	char str[] = "an array of characters"; 
+	char arrofstr[][4] = {"str1", "str2", "str3", "str4"};
+	print1darr(intarr, sizeof(intarr)/sizeof(int), "int");
+
+
 }
