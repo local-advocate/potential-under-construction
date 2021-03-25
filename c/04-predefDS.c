899ca70537e8edb58fce67478a4f1f66b7d0206b
the-apprentice <theapprentice@ta.com>
2021-03-25 20:34:24 +0000

    continue tmrw

--- c/04-predefDS.c
+++ c/04-predefDS.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+// cont: array inside struct (use memcpy?), structure padding and packing unions, dynamic mem, threads
 
 int main(){
 
@@ -17,15 +18,15 @@ int main(){
 	};
 
 	typedef struct {
-		unsigned int cores : 5;		// max 5 bits
-		float weight;
+		unsigned int cores : 5;		// max 5 bits (not enforced. will convert/truncate)
+		char company[10];		// arrays must have specific size inside struct
 		unsigned int mem   : 5; 	// in gb
 	} laptop;
 
 	struct health h1;
 	h1.height = 78; h1.weight = 190.88; h1.bloodtype = "o+";
 
-	laptop l1 = {4, 10.99, 8};
-	laptop l2 = {.cores = 2, .mem = 4, .weight = 3.99};
+	laptop l1 = {4, "asus", 8};
+	laptop l2 = {.cores = 2, .mem = 4, .company = "nokia"};
 
 }
