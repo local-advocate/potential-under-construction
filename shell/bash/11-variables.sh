e70fe761ccd9981f645425c3f09cdbecc2030ca6
the-apprentice <theapprentice@ta.com>
2021-03-25 10:47:43 +0000

    cont https://tldp.org/LDP/abs/html/varassignment.html

--- shell/bash/11-variables.sh
+++ shell/bash/11-variables.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+#----int-var----#
 intvar=10						# w/ _ interpreted as command
 echo $intvar						# $var: gives value of var. same as ${var}
 echo "intvar is ${intvar}"				# "" no interfernce with var substitution
 echo 'intvar is $intvar'				# '' does not substitute $var's value
-
 echo
 
+
+#----str-var----#
 strvar="A b   c     D"					# "" required if _ in variable value
 echo $strvar						# does not preserve spaces. same with ${var}
 echo "$strvar"						# preserves spaces
 echo 'strvar is $strvar'				# again no subsitution
 unset strvar						# sets its value to null
-
-
 echo
 
+
+#----null-var----#
 nullvar=						# null value (same result as after unsetting)
 if [ -z $nullvar ]; then				# -z checks for null value (works for strvar too)
 	echo '$nullvar is null'
 fi
+echo
+
+
+#----plain-var----#
+
