5646c0dbbba4f01a316582b564a37f424dad7804
the-apprentice <theapprentice@ta.com>
2021-03-25 20:24:28 +0000

    added structures

--- c/04-predefDS.c
+++ c/04-predefDS.c
@@ -1,40 +1,31 @@
 #include <stdio.h>
 
-enum arrtype {INT, CHAR, DOUBLE};
-
-void print1darr (int *p, int size, int type){
-	switch(type) {
-
-		case INT:
-			for (int i =0; i < size; i++) {
-				printf("int-arr[%d]: %d\t", i, *(p+i));
-			}
-			printf("\n");
-			break;
-
-		case DOUBLE:
-			for (int i =0; i < size; i++) {
-				printf("double-arr[%d]: %lf\t", i, *(p+i));
-			}
-			printf("\n");
-			break;
-
-	}
-}
-
-
-
 int main(){
 
 	/*----ARRAYS----*/
-	int intarr[5] = {0, 1, 2, 3, 4};
+	int intarr[5] = {0, 1, 2, 3, 4};				// 1d array
 	double doubarr[] = {1.1, 2.22, 3.333, 4.4444};
-	char str[] = "an array of characters"; 
-	char arrofstr[][4] = {"str1", "str2", "str3", "str4"};
+	char str[] = "an array of characters";				// size = sizeof(str)/sizeof(char) 
+	char arrofstr[][4] = {"str1", "str2", "str3", "str4"};		// 2d arr 
 
 
-	print1darr(intarr, sizeof(intarr)/sizeof(int), INT);
-	print1darr(doubarr, sizeof(doubarr)/sizeof(double), DOUBLE);
+	/*----STRUCT----*/
+	struct health {
+		int height;
+		double weight;
+		char *bloodtype;
+	};
 
+	typedef struct {
+		unsigned int cores : 5;		// max 5 bits
+		float weight;
+		unsigned int mem   : 5; 	// in gb
+	} laptop;
+
+	struct health h1;
+	h1.height = 78; h1.weight = 190.88; h1.bloodtype = "o+";
+
+	laptop l1 = {4, 10.99, 8};
+	laptop l2 = {.cores = 2, .mem = 4, .weight = 3.99};
 
 }
